1f7153f51026cd092fb9d10f55cccb720ccbff13
Andreas Rozek <A.Rozek@gmx.de>
2025-07-28 08:42:54 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-import { IntlProvider } from './components/provider'
+import { IntlProvider,Context } from './components/provider'
 import { Text } from './components/text'
 import { MarkupText } from './components/markup-text'
 import { Localizer } from './components/localizer'
+import translate from './lib/translate'
 
-export { IntlProvider, Text, MarkupText, Localizer }
+export {
+  IntlProvider, Text, MarkupText, Localizer,
+  Context as IntlContext, translate
+}
